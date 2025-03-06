2776a533ecf53499ae556177062815e88e8877fa
saket-gupta99 <saketg54@gmail.com>
2025-03-07 01:01:59 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
