2b6141ea9703869a4b17ca3590d3e91885a9a962
saket-gupta99 <saketg54@gmail.com>
2025-03-07 00:38:44 +0530

    updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,8 +27,12 @@ app.use(
   cors({
     origin: FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
+app.options("*", cors());  // Allows preflight requests
+
 
 connectToDB()
   .then(() => {
